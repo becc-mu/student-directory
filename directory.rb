5bafa2c6aceb7d81541c848089de3b8ff91afb9d
becc-mu <becc.mulugeta@gmail.com>
2018-09-24 18:39:41 +0100

    Added student array and methords

--- directory.rb
+++ directory.rb
@@ -1,19 +1,30 @@
-#printed list of students
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#finally, we print the total number of students
-print "Overall, we have \n"
-#it's important that print() dosen't add new line characters
-print " 11 \n"
-print " great students"
+# let's put all students into an array
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
